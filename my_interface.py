c4b49121732e2580fa4789acf7200e375b1ab797
Shraddha Bollapragada <l.s.s.bollapragada@student.tudelft.nl>
2023-02-28 12:20:26 +0100

    keyboard and joystick threading

--- my_interface.py
+++ my_interface.py
@@ -2,7 +2,6 @@
 from curses.ascii import isalpha
 from multiprocessing.connection import Listener
 import struct
-# import this
 import pygame
 import time
 import sys
@@ -38,96 +37,127 @@ done= False
 clock = pygame.time.Clock()
 
 #create global variables to store the joystick values and initialize them to zero
+global drone_mode
 global pitch
 global yaw 
 global lift 
 global roll 
+global P
+global P1
+global P2
 pitch =0.0; yaw =0.0; lift =0.0; roll =0.0
+drone_mode = 0
+P = 0.0
+P1 = 0.0
+P2 = 0.0
 
 
 #make a function for joystick data
 def get_joystick_data():
     #  done=False
-    #  while (done!=True):
-     for event in pygame.event.get():
-        print(event)
+ while (True):
+    global drone_mode
+    global pitch, yaw, lift, roll
+    for event in pygame.event.get():
+        print(event.type)
         if event.type == pygame.JOYBUTTONDOWN:
                     print("Button:",event.dict['button'],"pressed")
                     if event.dict['button'] == 0:
-                        global done
-                        done = True
+                        # global done
+                        # done = True
                         # return done
-                        break
+                        drone_mode = 9
+                        print("stopping")
+                        continue
         elif event.type == pygame.JOYAXISMOTION:
                 for i in range(axes):
-                    global pitch 
                     pitch = joystick.get_axis(1)
                     print("Pitch:" , pitch)
-                    global yaw
+                    # global yaw
                     yaw = joystick.get_axis(2)
                     print("Yaw:" , yaw)
-                    global lift
+                    # global lift
                     lift = joystick.get_axis(3)
                     print("Lift:" , lift)
-                    global roll
+                    # global roll
                     roll = joystick.get_axis(0)
                     print("Roll:" , roll)
-                    # continue
-                # return done
-#make a function for getting keyboard data
+                    continue
+            # return done
+    #make a function for getting keyboard data
 def get_keyboard_data():
         # done=False
+        global drone_mode
         global pitch, yaw, lift, roll
+        global P, P1, P2
         with keyboard.Events() as events:
             # print(keyboard.KeyCode(char="a"))
             for event in events:
                 # print(type(event.key.char))
                 if event.key == keyboard.Key.esc or event.key == keyboard.Key.space:
-                 global done
-                 done=True
+                   drone_mode = 9
+                #  global done
+                #  done=True
                 #  return done
-                 break
-                if  event.key == keyboard.Key.up or event.key== keyboard.Key.down or event.key== keyboard.Key.left or event.key== keyboard.Key.right:
-                    if event.key == keyboard.Key.up and type(event) == keyboard.Events.Press:
-                        if pitch <= 0.9:
-                         pitch = pitch + 0.1
-                        print("Pitch: ", pitch)
-                    elif event.key == keyboard.Key.down and type(event) == keyboard.Events.Press:
-                        if pitch >= -0.9:
-                         pitch = pitch - 0.1
-                        print("Pitch: ", pitch)
-                    elif event.key == keyboard.Key.left and type(event) == keyboard.Events.Press:
-                        if roll <= 0.9:
-                         roll = roll + 0.1
-                        print("Roll: ", roll)
-                    elif event.key == keyboard.Key.right and type(event) == keyboard.Events.Press:
-                        if roll >=-0.9:
-                         roll = roll - 0.1
-                        print("Roll: ", roll) 
-                else:
-                     if event.key == keyboard.KeyCode(char="a") and type(event) == keyboard.Events.Press:
-                            if lift <= 0.9:
-                             lift = lift + 0.1
-                            print("Thrust: ", lift)  
-                     elif event.key == keyboard.KeyCode(char="z") and type(event) == keyboard.Events.Press:
-                            if lift >= -0.9:
-                             lift = lift - 0.1
-                            print("Thrust: ", lift)     
-                     elif event.key == keyboard.KeyCode(char="q") and type(event) == keyboard.Events.Press:
-                            if yaw <= 0.9:
-                             yaw = yaw + 0.1
-                            print("Yaw: ", yaw)
-                     elif event.key == keyboard.KeyCode(char="w") and type(event) == keyboard.Events.Press:
-                            if yaw >= -0.9:
-                             yaw = yaw - 0.1 
-                            print("Yaw: ", yaw)
-                     elif event.key == keyboard.KeyCode(char ='1') and type(event) == keyboard.Events.Press:
-                         print( "Mode1")
-                     elif event.key == keyboard.KeyCode(char ='0') and type(event) == keyboard.Events.Press:
-                         print( "Mode0")
-                     else:
-                         break
-        # return done
+                
+                #  break
+                elif event.key == keyboard.Key.up and type(event) == keyboard.Events.Press:
+                    if pitch <= 0.9:
+                        pitch = pitch + 0.1
+                    print("Pitch: ", pitch)
+                elif event.key == keyboard.Key.down and type(event) == keyboard.Events.Press:
+                    if pitch >= -0.9:
+                        pitch = pitch - 0.1
+                    print("Pitch: ", pitch)
+                elif event.key == keyboard.Key.left and type(event) == keyboard.Events.Press:
+                    if roll <= 0.9:
+                        roll = roll + 0.1
+                    print("Roll: ", roll)
+                elif event.key == keyboard.Key.right and type(event) == keyboard.Events.Press:
+                    if roll >=-0.9:
+                        roll = roll - 0.1
+                    print("Roll: ", roll) 
+                elif event.key == keyboard.KeyCode(char="a") and type(event) == keyboard.Events.Press:
+                    if lift <= 0.9:
+                        lift = lift + 0.1
+                    print("Thrust: ", lift)  
+                elif event.key == keyboard.KeyCode(char="z") and type(event) == keyboard.Events.Press:
+                    if lift >= -0.9:
+                        lift = lift - 0.1
+                    print("Thrust: ", lift)     
+                elif event.key == keyboard.KeyCode(char="q") and type(event) == keyboard.Events.Press:
+                    if yaw <= 0.9:
+                        yaw = yaw + 0.1
+                    print("Yaw: ", yaw)
+                elif event.key == keyboard.KeyCode(char="w") and type(event) == keyboard.Events.Press:
+                    if yaw >= -0.9:
+                        yaw = yaw - 0.1 
+                    print("Yaw: ", yaw)
+                elif event.key == keyboard.KeyCode(char ='1') and type(event) == keyboard.Events.Press:
+                    drone_mode= 1
+                    print( "Mode1")
+                elif event.key == keyboard.KeyCode(char ='0') and type(event) == keyboard.Events.Press:
+                    drone_mode = 0
+                    print( "Mode0")
+                elif event.key == keyboard.KeyCode(char="u") and type(event) == keyboard.Events.Press:
+                        P = P + 0.1 
+                        print("P: ", P)
+                elif event.key == keyboard.KeyCode(char="j") and type(event) == keyboard.Events.Press:
+                        P = P - 0.1 
+                        print("P: ", P)
+                elif event.key == keyboard.KeyCode(char="i") and type(event) == keyboard.Events.Press:
+                        P1 = P1 + 0.1 
+                        print("P1: ", P1)
+                elif event.key == keyboard.KeyCode(char="k") and type(event) == keyboard.Events.Press:
+                        P1 = P1 - 0.1 
+                        print("P1: ", P1)
+                elif event.key == keyboard.KeyCode(char="o") and type(event) == keyboard.Events.Press:
+                        P2 = P2 + 0.1 
+                        print("P2: ", P2)
+                elif event.key == keyboard.KeyCode(char="l") and type(event) == keyboard.Events.Press:
+                        P2 = P2 - 0.1 
+                        print("P2: ", P2)
+        # return done1
         #need to add for yaw control and pitch control ??aaa
 #function to get the data from the joystick or keyboard
 def get_data():
@@ -137,12 +167,12 @@ def get_data():
 # -------- Main Program Loop -----------
 # while done!=True:
         # print("in python code")
+thread1=threading.Thread(target=get_joystick_data)
 if (joysticks > 0):
- thread1=threading.Thread(target=get_joystick_data)
  thread1.start()
 thread2=threading.Thread(target=get_keyboard_data)
 thread2.start()
-        # if(joysticks > 0):
+        # if(joysticks >aaaaappll 0):
             # done=get_joystick_data()
             # print(done)
             # print(get_data())
@@ -150,7 +180,7 @@ thread2.start()
             # done=get_keyboard_data()
             # print(get_data())
 # if (done==True):
-    
-thread1.join()
+if (thread1.is_alive()):
+    thread1.join()
 thread2.join()
 print(get_data()) 
