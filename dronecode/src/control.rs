bdd5c1daaea29ed0337d81d1dfb27d239b5dc84b
Tony <tonyyunyang@outlook.com>
2023-03-02 17:03:08 +0100

    New

--- dronecode/src/control.rs
+++ dronecode/src/control.rs
@@ -12,16 +12,18 @@ use tudelft_quadrupel::{block, initialize, uart};
 use tudelft_quadrupel::led::Led::{Blue, Red, Green, Yellow};
 use tudelft_quadrupel::motor::get_motors;
 use tudelft_quadrupel::mpu::{read_dmp_bytes, read_raw};
-use tudelft_quadrupel::time::{set_tick_frequency, wait_for_next_tick, Instant};
+use tudelft_quadrupel::time::{set_tick_frequency, wait_for_next_tick, Instant, delay_us_assembly, delay_ms_assembly};
 use tudelft_quadrupel::uart::{send_bytes, receive_bytes};
 
 use postcard::Error;
+
 
 pub fn control_loop() -> ! {
     set_tick_frequency(100);
     let mut last = Instant::now();
     let mut test: u8 = 0;
     let mut ack = 0b0000_0000;
+    let mut buf = [0u8; 255];
 
     // let mut logger = logger::BlackBoxLogger::new();
     // logger.start_logging();
@@ -38,36 +40,11 @@ pub fn control_loop() -> ! {
         let pres = read_pressure();
 
         // the code below is for receiving the message from the host
-        if i % 100 == 0 {
-            let mut buf: Vec<u8> = Vec::new();
-            let mut messsage_buffer: Vec<u8> = Vec::new();
-            let mut received_bytes_count = 0; // the size of the message should be exactly 40 bytes, since we are using fixed size
-            let mut start_receiving = false;
-
+        if i % 10 == 0 {
             let num = receive_bytes(&mut buf);
-            if num == 0 {
+            if num != 0 {
+                Yellow.on();
                 ack = 0b1111_1111;
-                for i in 0..num {
-                    let received_byte = buf[i];
-                    if received_byte == 0x7b && start_receiving == false {
-                        messsage_buffer.clear();
-                        start_receiving = true;
-                    }
-                    if start_receiving == true {
-                        messsage_buffer.push(received_byte);
-                        received_bytes_count += 1;
-                    }
-                    if received_byte == 0x7d && start_receiving == true {
-                        if received_bytes_count != 12{
-                            ack = 0b0000_0000;
-                        } else if received_bytes_count == 12 {
-                            let nice_received_message = HostProtocol::format_message(&mut messsage_buffer);
-                            ack = verify_message(&nice_received_message);
-                            received_bytes_count = 0;
-                            start_receiving = false;
-                        }
-                    }
-                }
             }
         }
 
@@ -90,44 +67,127 @@ pub fn control_loop() -> ! {
             let mut message = Vec::new();
             message_to_host.form_message(&mut message);
             send_bytes(&message);
-
-            /**********************
-             **** Data logging ****
-             **********************/
-
-            // First fill in the data log form
-            // Then do logger.log_data(&data_log_form);
-            // let data_log_form = DroneLogData {
-            //     timestamp: now.ns_since_start(),
-            //     accel: [accel.x, accel.y, accel.z],
-            //     motor: motors,
-            //     ypr: [ypr.yaw.to_bits(), ypr.pitch.to_bits(), ypr.roll.to_bits()],
-            //     bat,
-            //     pres,
-            //     cpu_usage: 0,
-            //     ram_usage: 0,
-            //     flash_usage: 0,
-            //     mode: test,
-            // };
-
-            // logger.log(&data_log_form);
-
-            // if (i % 1000) == 0 {
-            //     // Stop logging every 10 seconds
-            //     logger.stop_logging();
-            // }
+            ack = 0b0000_0000;
+            Yellow.off();
         }
-
-        // Stop the logger
-
-        // Read from the flash memory
-
-        // wait until the timer interrupt goes off again
-        // based on the frequency set above
         wait_for_next_tick();
     }
     unreachable!();
 }
+
+// pub fn control_loop() -> ! {
+//     set_tick_frequency(100);
+//     let mut last = Instant::now();
+//     let mut test: u8 = 0;
+//     let mut ack = 0b0000_0000;
+
+//     // let mut logger = logger::BlackBoxLogger::new();
+//     // logger.start_logging();
+
+//     for i in 0.. {
+//         delay_ms_assembly(1000);
+//         let now = Instant::now();
+//         let dt = now.duration_since(last);
+//         last = now;
+//         let motors = get_motors();
+//         let quaternion = block!(read_dmp_bytes()).unwrap();
+//         let ypr = YawPitchRoll::from(quaternion);
+//         let (accel, _) = read_raw().unwrap();
+//         let bat = read_battery();
+//         let pres = read_pressure();
+
+//         // the code below is for receiving the message from the host
+//         if i % 100 == 0 {
+//             let mut buf = [0u8; 255];
+//             let mut messsage_buffer: Vec<u8> = Vec::new();
+//             let mut received_bytes_count = 0; // the size of the message should be exactly 40 bytes, since we are using fixed size
+//             let mut start_receiving = false;
+
+//             let num = receive_bytes(&mut buf);
+//             if num != 0 {
+//                 Yellow.on();
+//                 ack = 0b1111_1111;
+//                 // for i in 0..num {
+//                 //     let received_byte = buf[i];
+//                 //     if received_byte == 0x7b && start_receiving == false {
+//                 //         messsage_buffer.clear();
+//                 //         start_receiving = true;
+//                 //     }
+//                 //     if start_receiving == true {
+//                 //         messsage_buffer.push(received_byte);
+//                 //         received_bytes_count += 1;
+//                 //     }
+//                 //     if received_byte == 0x7d && start_receiving == true {
+//                 //         if received_bytes_count != 12{
+//                 //             ack = 0b0000_0000;
+//                 //         } else if received_bytes_count == 12 {
+//                 //             let nice_received_message = HostProtocol::format_message(&mut messsage_buffer);
+//                 //             ack = verify_message(&nice_received_message);
+//                 //             received_bytes_count = 0;
+//                 //             start_receiving = false;
+//                 //         }
+//                 //     }
+//                 // }
+//             }
+//         }
+
+//         // the code below is for sending the message to the host
+//         if i % 100 == 0 {
+//             // Create an instance of the Drone Protocol struct
+//             test += 1;
+//             let message_to_host = DeviceProtocol::new(
+//                 test,
+//                 dt.as_millis() as u16,
+//                 motors,
+//                 [ypr.yaw, ypr.pitch, ypr.roll],
+//                 [accel.x, accel.y, accel.z],
+//                 bat,
+//                 pres,
+//                 ack,
+//             );
+
+//             // Form the message waiting to be sent to the host
+//             let mut message = Vec::new();
+//             message_to_host.form_message(&mut message);
+//             send_bytes(&message);
+
+//             /**********************
+//              **** Data logging ****
+//              **********************/
+
+//             // First fill in the data log form
+//             // Then do logger.log_data(&data_log_form);
+//             // let data_log_form = DroneLogData {
+//             //     timestamp: now.ns_since_start(),
+//             //     accel: [accel.x, accel.y, accel.z],
+//             //     motor: motors,
+//             //     ypr: [ypr.yaw.to_bits(), ypr.pitch.to_bits(), ypr.roll.to_bits()],
+//             //     bat,
+//             //     pres,
+//             //     cpu_usage: 0,
+//             //     ram_usage: 0,
+//             //     flash_usage: 0,
+//             //     mode: test,
+//             // };
+
+//             // logger.log(&data_log_form);
+
+//             // if (i % 1000) == 0 {
+//             //     // Stop logging every 10 seconds
+//             //     logger.stop_logging();
+//             // }
+//         }
+
+//         // Stop the logger
+
+//         // Read from the flash memory
+
+//         // wait until the timer interrupt goes off again
+//         // based on the frequency set above
+//         wait_for_next_tick();
+//     }
+//     unreachable!();
+// }
 
 fn verify_message(message: &HostProtocol) -> u8  {
     // we check the start bit and the end bit first
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -13,66 +13,89 @@ use fixed::{
 };
 use std::sync::mpsc::{channel, Receiver, Sender};
 
+
 fn main() {
     let file = args().nth(1);
     let port = upload_file_or_stop(PortSelector::AutoManufacturer, file);
-    let port_ref1 = port.clone();
-    let port_ref2 = port.clone();
-    let port_ref3 = port.clone();
-    let port_ref4 = port.clone();
+    let serial = SerialPort::open(&port, 115200).unwrap();
 
-    // set up channels for communication between threads
-    let (uart_state_tx, uart_state_rx): (Sender<host_state>, Receiver<host_state>) = channel();
-    let (receiver_state_tx, receiver_state_rx): (Sender<host_state>, Receiver<host_state>) = channel();
-    let (sender_state_tx, sender_state_rx): (Sender<host_state>, Receiver<host_state>) = channel();
-    let (received_message_tx, received_message_rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
-    let (sending_message_tx, sending_message_rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
-    let (censor_tx, censor_rx): (Sender<HostProtocol>, Receiver<HostProtocol>) = channel();
+    sleep(Duration::from_millis(1000));
+    let mut buf = [0u8; 255];
 
-    let receiver_uart_state_tx = uart_state_tx.clone();
-    let censor_uart_state_tx = uart_state_tx.clone();
-    let sender_uart_state_tx = uart_state_tx.clone();
-
-    // start_interface(&port);
-
-    // below is for debugging purpose
-    // loop {
-    //     if let Ok(num) = serial.read(&mut buf) {
-    //         print!("\n The size of the messgae is: {} \n", num);
-
-    //         print!("The Message is:\n");
-
-    //         for i in 0..num {
-    //             print!("{:02x} ", buf[i]);
-    //         }
-
-    //         print!("\n");
-
-    //     }
-    // }
-
-    let uart_handler = thread::spawn(move || {
-        loop_uart_handler(port_ref1, uart_state_rx, receiver_state_tx, sender_state_tx, received_message_rx, censor_rx, sending_message_tx);
-    });
-    
-    let receive_device_message = thread::spawn(move || {
-        loop_receive_device_message(port_ref2, receiver_state_rx, receiver_uart_state_tx, received_message_tx);
-    });
-
-    let read_user_input = thread::spawn(move || {
-        loop_read_user_input(port_ref3, censor_uart_state_tx, censor_tx);
-    });
-    
-    let send_host_command = thread::spawn(move || {
-        loop_send_host_command(port_ref4, sender_state_rx, sender_uart_state_tx, sending_message_rx);
-    });
-
-    uart_handler.join().unwrap();
-    receive_device_message.join().unwrap();
-    read_user_input.join().unwrap();
-    send_host_command.join().unwrap();
+    loop {
+        let message = [1u8;5];
+        serial.write(&message).unwrap();
+        if let Ok(num) = serial.read(&mut buf) {
+            print!("\n The size of the messgae is: {} \n", num);
+            print!("The Message is:\n");
+            for i in 0..num {
+                print!("{:02x} ", buf[i]);
+            }
+            print!("\n");
+        }
+    }
 
 }
+// fn main() {
+//     let file = args().nth(1);
+//     let port = upload_file_or_stop(PortSelector::AutoManufacturer, file);
+//     let port_ref1 = port.clone();
+//     let port_ref2 = port.clone();
+//     let port_ref3 = port.clone();
+//     let port_ref4 = port.clone();
+
+//     // set up channels for communication between threads
+//     let (uart_state_tx, uart_state_rx): (Sender<host_state>, Receiver<host_state>) = channel();
+//     let (receiver_state_tx, receiver_state_rx): (Sender<host_state>, Receiver<host_state>) = channel();
+//     let (sender_state_tx, sender_state_rx): (Sender<host_state>, Receiver<host_state>) = channel();
+//     let (received_message_tx, received_message_rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
+//     let (sending_message_tx, sending_message_rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
+//     let (censor_tx, censor_rx): (Sender<HostProtocol>, Receiver<HostProtocol>) = channel();
+
+//     let receiver_uart_state_tx = uart_state_tx.clone();
+//     let censor_uart_state_tx = uart_state_tx.clone();
+//     let sender_uart_state_tx = uart_state_tx.clone();
+
+//     // start_interface(&port);
+
+//     // below is for debugging purpose
+//     // loop {
+//     //     if let Ok(num) = serial.read(&mut buf) {
+//     //         print!("\n The size of the messgae is: {} \n", num);
+
+//     //         print!("The Message is:\n");
+
+//     //         for i in 0..num {
+//     //             print!("{:02x} ", buf[i]);
+//     //         }
+
+//     //         print!("\n");
+
+//     //     }
+//     // }
+
+//     let uart_handler = thread::spawn(move || {
+//         loop_uart_handler(port_ref1, uart_state_rx, receiver_state_tx, sender_state_tx, received_message_rx, censor_rx, sending_message_tx);
+//     });
+    
+//     let receive_device_message = thread::spawn(move || {
+//         loop_receive_device_message(port_ref2, receiver_state_rx, receiver_uart_state_tx, received_message_tx);
+//     });
+
+//     let read_user_input = thread::spawn(move || {
+//         loop_read_user_input(port_ref3, censor_uart_state_tx, censor_tx);
+//     });
+    
+//     let send_host_command = thread::spawn(move || {
+//         loop_send_host_command(port_ref4, sender_state_rx, sender_uart_state_tx, sending_message_rx);
+//     });
+
+//     uart_handler.join().unwrap();
+//     receive_device_message.join().unwrap();
+//     read_user_input.join().unwrap();
+//     send_host_command.join().unwrap();
+
+// }
 
 enum host_state {
     idle,
@@ -140,11 +163,11 @@ fn loop_uart_handler(port: PathBuf, state: Receiver<host_state>, receiver_state:
 
 fn loop_receive_device_message(port: PathBuf, state: Receiver<host_state>, uart_state: Sender<host_state>, message: Sender<Vec<u8>>) {
     let mut serial = SerialPort::open(port, 115200).unwrap();
-    serial.set_read_timeout(Duration::from_secs(4)).unwrap();
     let mut received_bytes_count = 0; // the size of the message should be exactly 40 bytes, since we are using fixed size
     let mut buf = [0u8; 255];
     let mut message_buffer = Vec::new();
     let mut start_receiving = false;
+    sleep(Duration::from_millis(1000));
     'outer: loop {
         uart_state.send(host_state::idle).unwrap();
         let receiver_state = state.recv();
@@ -201,7 +224,7 @@ fn loop_receive_device_message(port: PathBuf, state: Receiver<host_state>, uart_
                                 
                             },
                             Err(err) => {
-                                println!("\nAn error occured: {}\n", err);
+                                // println!("\nAn error occured: {}\n", err);
                                 continue 'outer;
                             },
                         }
@@ -232,7 +255,6 @@ fn loop_read_user_input(port: PathBuf, uart_state: Sender<host_state>, command: 
 
 fn loop_send_host_command(port: PathBuf, state: Receiver<host_state>, uart_state: Sender<host_state>, sending_message: Receiver<Vec<u8>>) {
     let mut serial = SerialPort::open(port, 115200).unwrap();
-    serial.set_write_timeout(Duration::from_secs(1)).unwrap();
     'outer: loop {
         uart_state.send(host_state::idle).unwrap();
         let sender_state = state.recv();
