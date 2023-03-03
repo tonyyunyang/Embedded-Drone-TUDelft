d7a5edc5752b04d99aade926ff73503225f2d1b5
Tony <tonyyunyang@outlook.com>
2023-03-03 14:47:26 +0100

    Interface merged sucessfully into communication. Pipeline passed.

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -32,57 +32,52 @@ fn start_interface(port: &Path) {
     cmd
         // there must be a `my_interface.py` file of course
         .arg("my_interface.py");
-        // pass the serial port as a command line parameter to the python program
-        // .arg(port.to_str().unwrap());
-        unsafe
-        {
-            eprint!("{:?}", cmd.output().unwrap().stdout);
-        }
-   
+    // pass the serial port as a command line parameter to the python program
+    // .arg(port.to_str().unwrap());
+    unsafe {
+        eprint!("{:?}", cmd.output().unwrap().stdout);
+    }
+
     match cmd.output() {
         Err(e) => {
             eprintln!("{}", e);
             exit(1);
         }
-        Ok(i)=>
-        {
-            unsafe{
-
+        Ok(i) => {
+            unsafe {
                 eprintln!("{}", String::from_utf8_unchecked(i.stdout));
                 exit(1);
             }
-         if !i.status.success() 
-         {
-            exit(i.status.code().unwrap_or(1))
-         }
+            if !i.status.success() {
+                exit(i.status.code().unwrap_or(1))
+            }
         }
         Ok(_) => {}
     }
-}
-#[allow(unused)]
-fn get_joystick_data_rust_og() {
-    let mut cmd = Command::new("python");
-    cmd
-        // there must be a `my_interface.py` file of course
-        .arg("-c")
-        .arg("'from my_interface import *; get_joystick_data()'");
-        // pass the serial port as a command line parameter to the python program
-       //get back pitch, roll, yaw, throttle from python;
-    match cmd.output() {
-        Err(e) => {
-            eprintln!("{}", e);
-            exit(1);
-        }
-        Ok(i) if !i.status.success() =>
-        {
-            i.stdout;
-            exit(i.status.code().unwrap_or(1));
-        }
-        Ok(_) => {}
-    }
-    
 }
 
+
+// #[allow(unused)]
+// fn get_joystick_data_rust_og() {
+//     let mut cmd = Command::new("python");
+//     cmd
+//         // there must be a `my_interface.py` file of course
+//         .arg("-c")
+//         .arg("'from my_interface import *; get_joystick_data()'");
+//     // pass the serial port as a command line parameter to the python program
+//     //get back pitch, roll, yaw, throttle from python;
+//     match cmd.output() {
+//         Err(e) => {
+//             eprintln!("{}", e);
+//             exit(1);
+//         }
+//         Ok(i) if !i.status.success() => {
+//             i.stdout;
+//             exit(i.status.code().unwrap_or(1));
+//         }
+//         Ok(_) => {}
+//     }
+// }
 
 // fn get_joystick_data_rust()
 // {
