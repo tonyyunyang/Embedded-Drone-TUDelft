439d7952b138ac32a838c7869f2c4c660dc49b87
GiovanniLin <g.lin@student.tudelft.nl>
2023-03-08 21:26:29 +0100

    Added Permissions field and comments

--- dronecode/src/control/state_machine.rs
+++ dronecode/src/control/state_machine.rs
@@ -2,8 +2,6 @@
 // The state machine is a finite state machine (FSM) that is used to control the drone.
 
 use crate::control::state_machine::State::Safety;
-use crate::panic;
-use alloc::string::String;
 use core::clone::Clone;
 
 // Define the possible states of the state machine.
@@ -42,7 +40,22 @@ pub enum State {
 pub struct StateMachine {
     state: State,
     operation_ready: bool,
+    permissions: Permissions,
     // Add more fields here if needed such as data to be stored in the state machine.
+}
+
+// Yaw, Pitch, Roll and Height are all for the controller.
+// Wireless can also be read from the mode, but having it as Permission might be useful.
+// Sensors specify whether the mode should process the sensor readings into the calculations.
+// For example, manual mode would not use sensor data -> sensors: false.
+#[derive(Clone)]
+pub struct Permissions {
+    controller: bool,
+    yaw_control: bool,
+    pitch_roll_control: bool,
+    height_control: bool,
+    wireless: bool,
+    sensors: bool,
 }
 
 // Implement methods for the state machine.
@@ -52,6 +65,14 @@ impl StateMachine {
         StateMachine {
             state: State::Safety,
             operation_ready: false,
+            permissions: Permissions {
+                controller: false,
+                yaw_control: false,
+                pitch_roll_control: false,
+                height_control: false,
+                wireless: false,
+                sensors: false,
+            },
         }
     }
 
@@ -61,47 +82,162 @@ impl StateMachine {
     }
 
     // Transition the state machine to a new state.
-    pub fn transition(&mut self, next_state: State) {
+    // Returns true for a proper transition and false for an illegal transition.
+    // This value can then be communicated back to the PC.
+    pub fn transition(&mut self, next_state: State) -> bool {
         if self.state != next_state {
             match next_state {
-                State::Safety => self.state = Safety,
+                State::Safety => self.transition_safe(),
                 State::Panic => self.transition_panic(),
-                State::Manual => self.state = State::Manual,
+                State::Manual => self.transition_manual(),
                 State::Calibrate => self.transition_calibrate(),
                 State::Yaw | State::Full | State::Raw | State::Height | State::Wireless => {
                     self.transition_operation(next_state)
                 }
             }
+        } else {
+            false
         }
     }
 
-    fn transition_panic(&mut self) {
-        self.state = State::Panic;
-        // TODO Panic handling
+    // Safe mode should do nothing so everything is false.
+    fn transition_safe(&mut self) -> bool {
+        self.state = State::Safety;
+        self.permissions.controller = false;
+        self.permissions.yaw_control = false;
+        self.permissions.pitch_roll_control = false;
+        self.permissions.height_control = false;
+        self.permissions.wireless = false;
+        self.permissions.sensors = false;
+        true
     }
 
-    fn transition_calibrate(&mut self) {
+    // Panic mode should also do nothing from the controller, might need the sensors (for now false).
+    fn transition_panic(&mut self) -> bool {
+        self.state = State::Panic;
+        self.permissions.controller = false;
+        self.permissions.yaw_control = false;
+        self.permissions.pitch_roll_control = false;
+        self.permissions.height_control = false;
+        self.permissions.wireless = false;
+        self.permissions.sensors = false;
+        // Reset the calibration flag if there was a panic.
+        self.operation_ready = false;
+        // TODO Panic handling
+        true
+    }
+
+    // Manual mode should accept all controller movements, but not use any sensor data.
+    fn transition_manual(&mut self) -> bool {
+        // Can only go into manual mode from safe mode.
+        if self.state == State::Safety {
+            self.state = State::Manual;
+            self.permissions.controller = true;
+            self.permissions.yaw_control = false;
+            self.permissions.pitch_roll_control = false;
+            self.permissions.height_control = false;
+            self.permissions.wireless = false;
+            self.permissions.sensors = false;
+            true
+        } else {
+            false
+        }
+    }
+
+    // Calibration mode should only accept sensor data, no controller movements.
+    fn transition_calibrate(&mut self) -> bool {
+        // Can only go into calibration mode from safe mode.
         if self.state == State::Safety {
             self.state = State::Calibrate;
+            self.permissions.controller = false;
+            self.permissions.yaw_control = false;
+            self.permissions.pitch_roll_control = false;
+            self.permissions.height_control = false;
+            self.permissions.wireless = false;
+            self.permissions.sensors = true;
+            true
         } else {
-            // TODO proper error handling
-            panic!("Improper transition, can only go to calibrate from safety.")
+            false
         }
     }
 
-    fn transition_operation(&mut self, next_state: State) {
+    // Checks whether calibration is done and then redirects to the required transition.
+    // All operating modes use the sensors for control loops.
+    fn transition_operation(&mut self, next_state: State) -> bool {
+        // Calibration flag check
         if self.operation_ready && self.state == Safety {
             match next_state {
-                State::Yaw => self.state = State::Yaw,
-                State::Full => self.state = State::Full,
-                State::Raw => self.state = State::Raw,
-                State::Height => self.state = State::Height,
-                State::Wireless => self.state = State::Wireless,
-                _ => unreachable!(),
+                State::Yaw => self.transition_yaw(),
+                State::Full => self.transition_full(),
+                State::Raw => self.transition_raw(),
+                State::Height => self.transition_height(),
+                State::Wireless => self.transition_wireless(),
+                _ => unreachable!(), // Match needs to be exhaustive, but this is unreachable due to match in transition()
             }
         } else {
-            // TODO proper error handling
-            panic!("Improper transition, can only operate if calibration is done from safety mode.")
+            false
         }
+    }
+
+    // Yaw control mode should enable the yaw control loop.
+    fn transition_yaw(&mut self) -> bool {
+        self.state = State::Yaw;
+        self.permissions.controller = true;
+        self.permissions.yaw_control = true;
+        self.permissions.pitch_roll_control = false;
+        self.permissions.height_control = false;
+        self.permissions.wireless = false;
+        self.permissions.sensors = true;
+        true
+    }
+
+    // Full control mode should enable the yaw control loop and the pitch-roll control loop.
+    fn transition_full(&mut self) -> bool {
+        self.state = State::Full;
+        self.permissions.controller = true;
+        self.permissions.yaw_control = true;
+        self.permissions.pitch_roll_control = true;
+        self.permissions.height_control = false;
+        self.permissions.wireless = false;
+        self.permissions.sensors = true;
+        true
+    }
+
+    // Raw sensor reading mode should add the filters, to the other control loops.
+    fn transition_raw(&mut self) -> bool {
+        self.state = State::Raw;
+        self.permissions.controller = true;
+        self.permissions.yaw_control = true;
+        self.permissions.pitch_roll_control = true;
+        self.permissions.height_control = false;
+        self.permissions.wireless = false;
+        self.permissions.sensors = true;
+        true
+    }
+
+    // On top of the two control loops and the filters, a height control loop is added.
+    fn transition_height(&mut self) -> bool {
+        self.state = State::Height;
+        self.permissions.controller = true;
+        self.permissions.yaw_control = true;
+        self.permissions.pitch_roll_control = true;
+        self.permissions.height_control = true;
+        self.permissions.wireless = false;
+        self.permissions.sensors = true;
+        true
+    }
+
+    // After all control loops and filters work, wireless mode can be used to fly the drone without the USB cable.
+    // Requires high stabilization of drone, since wireless bandwidth is lower.
+    // Only use this mode after all previous modes have been implemented.
+    fn transition_wireless(&mut self) -> bool {
+        self.state = State::Wireless;
+        self.permissions.controller = true;
+        self.permissions.yaw_control = true;
+        self.permissions.pitch_roll_control = true;
+        self.permissions.height_control = true;
+        self.permissions.wireless = true;
+        self.permissions.sensors = true;
+        true
     }
 }
